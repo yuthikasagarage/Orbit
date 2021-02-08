187f5c8044cbe5cb0910da3d16b83e0b528221d2
Yuthika Sagarage <sandamalsagarage@gmail.com>
2021-02-09 02:38:32 +0530

    added errorHandler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 5002;
 
